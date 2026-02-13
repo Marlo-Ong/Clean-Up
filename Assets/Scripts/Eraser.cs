c8d48722d4301b8b0577ea011f0a483b89b91067
Marlo Ongkingco <moon.ongkingco@gmail.com>
2026-02-12 16:02:44 -0800

    Don't clean when mouse is idle

--- Assets/Scripts/Eraser.cs
+++ Assets/Scripts/Eraser.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class Eraser : MonoBehaviour
 {
     [SerializeField, Range(0, 1)] private float eraseRadius;
+    Vector3 lastPosition;
 
     void OnTriggerStay2D(Collider2D other)
     {
+        Vector3 diff = this.transform.position - this.lastPosition;
+        this.lastPosition = this.transform.position;
+        if (diff.sqrMagnitude < 0.01f)
+            return;
+
         if (other.TryGetComponent<TintErasable>(out var tint))
             tint.EraseAtWorldPosition(transform.position, eraseRadius);
     }
